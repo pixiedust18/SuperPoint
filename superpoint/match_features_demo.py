1f93e127271389a6afab209b59b2e3b181c088b7
Krisha Bhambani <62170625+pixiedust18@users.noreply.github.com>
2021-08-17 03:00:02 +0530

    Update match_features_demo.py

--- superpoint/match_features_demo.py
+++ superpoint/match_features_demo.py
@@ -69,7 +69,7 @@ def compute_homography(matched_kp1, matched_kp2):
 
 def preprocess_image(img_file, img_size):
     
-    if len(image.shape) == 3:
+    if len(img.shape) == 3:
         img = cv2.imread(img_file, cv2.IMREAD_COLOR)
         img = cv2.resize(img, img_size)
         img_orig = img.copy()
